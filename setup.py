b55f57651148852a72eb3d2c2cd40865d6d111d8
Eleanor Howick <eleanor.howick@callaghaninnovation.govt.nz>
2020-05-18 16:52:37 +1200

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="roundness",
+    packages=find_packages(),
+    version="0.1.0",
+    description="analysis and ploting of RA2200 roundness measurements",
+    author="efh",
+)
